c1fd3c4a468dee24b5a6e2582948539128349c91
mayijian <mayijian@meituan.com>
2024-04-26 20:01:28 +0800

    feat: update config

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,4 +1,20 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-vim.opt.clipboard = "unnamedplus"
+
+-- vim.opt.clipboard = "unnamedplus"
+
+vim.api.nvim_create_autocmd("LspAttach", {
+  callback = function(opt)
+    local fname = vim.api.nvim_buf_get_name(opt.buf)
+    if #fname <= 0 then
+      return
+    end
+    local size = vim.fn.getfsize(fname) / 1024
+    if size >= 100 then
+      vim.schedule(function()
+        vim.lsp.buf_detach_client(opt.buf, opt.data.client_id)
+      end)
+    end
+  end,
+})
---
+++ lua/plugins/neotree.lua
@@ -0,0 +1,8 @@
+return {
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      open_files_do_not_replace_types = { "Trouble", "trouble", "qf", "Outline" },
+    },
+  },
+}
